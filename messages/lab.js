dbc1ba9fb0cc3af811327cfbf0fc05776a9fb70c
mseltzer94 <Michael@wr-130-64-184-172.medford.tufts.edu>
2015-03-07 23:25:27 -0500

    get pdata to parse

--- messages/lab.js
+++ messages/lab.js
@@ -1,1 +1,12 @@
-// Your JavaScript goes here...
+function parse(){
+  var req = new XMLHttpRequest();
+    var url = "data.json";
+    req.open("GET", url, true);
+    if(req.readyState == 4 && req.status == 200) {
+        data = JSON.parse(req.responseText);
+         for (var i = 0; i < data.length; i++){
+            console.log(data[i]);
+         }
+    }
+    req.send();  
+}
