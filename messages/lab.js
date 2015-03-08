9918f29db1a94477cb01db331b7f335f300895bd
mseltzer94 <Michael@wr-130-64-184-172.medford.tufts.edu>
2015-03-07 23:32:21 -0500

    debugging output

--- messages/lab.js
+++ messages/lab.js
@@ -3,12 +3,14 @@ function parse(){
     var url = "data.json";
     console.log("parsing");
     req.open("GET", url, true);
-    if(req.readyState == 4 && req.status == 200) {
-        console.log("HERE!");
+    req.addEventListener("load", complete, false);
+
+    req.send();  
+}
+
+function complete(evt){
+         console.log("HERE!");
         data = JSON.parse(req.responseText);
          for (var i = 0; i < data.length; i++){
-            console.log(data[i]);
-         }
-    }
-    req.send();  
+            console.log(data[i]);   
 }
