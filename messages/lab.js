cf43cc33c747683cd0e8a5f3deb5ed00d50190b2
mseltzer94 <Michael@wr-130-64-184-172.medford.tufts.edu>
2015-03-07 23:28:00 -0500

    debugging output

--- messages/lab.js
+++ messages/lab.js
@@ -3,6 +3,7 @@ function parse(){
     var url = "data.json";
     req.open("GET", url, true);
     if(req.readyState == 4 && req.status == 200) {
+        console.log("HERE!");
         data = JSON.parse(req.responseText);
          for (var i = 0; i < data.length; i++){
             console.log(data[i]);
