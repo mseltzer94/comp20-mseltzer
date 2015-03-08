3056985a9610a0f3f9b3408c1ddbf1aa6c5ca3cd
mseltzer94 <Michael@wr-130-64-184-172.medford.tufts.edu>
2015-03-07 23:40:03 -0500

    fix syntax

--- messages/lab.js
+++ messages/lab.js
@@ -14,7 +14,7 @@ function complete(evt){
         data = JSON.parse(req.responseText);
          for (var i = 0; i < data.length; i++){
             var message= document.createElement("LI");
-            var text = document.createTextNode(data[i].content + " " data[i].username);
+            var text = document.createTextNode(data[i].content + " " + data[i].username);
             message.appendChild(text);
             document.getElementById("messages").appendChild(message); 
          }
